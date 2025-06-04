41be0a3af212ade15d8a49b35be362de6aaee5b3
Seun Taiwo <lorddro1532@gmail.com>
2025-06-04 10:54:02 +0100

    fixed mobile bug

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,12 +10,14 @@ const pages: { text: string; url: string }[] = [
 
 export const Homepage = () => {
   return (
-    <div className="min-h-dvh flex font-cinzel flex-col text-white justify-center items-center bg-[#111]">
-      <h1 className="text-4xl">
-        Everyone has the right to freedom of thoughts
+    <div className="min-h-dvh flex font-cinzel flex-col text-white justify-center items-center bg-[#111] text-center space-y-4">
+      <h1 className="text-2xl lg:text-4xl">
+        Everyone has the right to freedom of thought.
       </h1>
-      <h1 className="text-4xl mb-4">This is where I express mine</h1>
-      <h1 className="text-4xl mb-4">Welcome to Dro's Playground</h1>
+      <h1 className="text-2xl lg:text-4xl mb-4">
+        This is where I express mine.
+      </h1>
+      <h1 className="text-2xl lg:text-4xl mb-4">Welcome to Dro's Playground</h1>
 
       <div className="mt-8 w-[80%] mx-auto grid grid-cols-3 gap-4 flex-wrap ">
         {pages.map((page, idx) => (
